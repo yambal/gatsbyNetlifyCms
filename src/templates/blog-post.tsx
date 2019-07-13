157f40e496954019e3a0d3ff8c1de64d89baafe7
June YAMAMOTO <june@yambal.net>
2019-07-13 12:46:46 +0900

    Revert "debug"
    This reverts commit 4c9d78f8b21b2c475953a4c988ac05a81a56d5f4.

--- src/templates/blog-post.tsx
+++ src/templates/blog-post.tsx
@@ -18,6 +18,7 @@ export const BlogPostTemplate = ({
 
   return (
     <section className="section">
+      {helmet || ''}
       <div className="container content">
         <div className="columns">
           <div className="column is-10 is-offset-1">
