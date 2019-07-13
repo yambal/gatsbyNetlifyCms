f9195ea7fa7d3fc0194abc0ba1e2818d6177d584
June YAMAMOTO <june@yambal.net>
2019-07-13 16:25:43 +0900

    Tailwind Container

---
+++ src/components/Container.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+interface iContainerProps {
+  id?: string
+  className?: string
+}
+
+const Container:React.SFC<iContainerProps> = (props) => {
+  const tailwindClasses = ['container', 'px-20', 'mx-auto']
+  const classNameClasses = props.className ? props.className.split(' ') : []
+  return (
+    <div id={props.id} className={tailwindClasses.concat(classNameClasses).join(' ')}>
+      {props.children}
+    </div>
+  )
+}
+
+export default Container
