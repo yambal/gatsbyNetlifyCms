7bf0f0134f04551ce9bc42a7e3596ee263a6c6df
June YAMAMOTO <june@yambal.net>
2019-08-03 23:26:03 +0900

    修正

--- src/GatsbyNodeToTemplate/web-services-blog-post.tsx
+++ src/GatsbyNodeToTemplate/web-services-blog-post.tsx
@@ -2,56 +2,14 @@ import React from 'react'
 import Layout from '../components/Layout'
 import Container from '../components/Container';
 import { graphql } from 'gatsby'
-import TemplateBlogPost from '../components/templates/TemplateBlogPost';
+import TemplateWebService from '../components/templates/TemplateWebService';
 
-interface iBlogPostProps{
-  loaction: {
-    pathname: string
-  }
-  data: {
-    markdownRemark: {
-      html: string
-      frontmatter: {
-        title: string
-        description: string
-        tags: string[]
-      }
-    }
-  },
-  pageContext: {
-    next?: {
-      fields: {
-        slug: string
-      }
-      frontmatter: {
-        title: string
-      }
-    }
-    prev?: {
-      fields: {
-        slug: string
-      }
-      frontmatter: {
-        title: string
-      }
-    }
-  }
+interface iWebServiceProps{
 }
 
-const BlogPost:React.SFC<iBlogPostProps> = props => {
+const WebService:React.SFC<iWebServiceProps> = props => {
   return (
     <Layout>
-      <TemplateBlogPost
-        title={props.data.markdownRemark.frontmatter.title}
-        description={props.data.markdownRemark.frontmatter.description}
-        content={props.data.markdownRemark.html}
-        tags={props.data.markdownRemark.frontmatter.tags}
-        isHtml={true}
-        prevTitle={props.pageContext.prev.frontmatter.title}
-        prevUri={props.pageContext.prev.fields.slug}
-        nextTitle={props.pageContext.next.frontmatter.title}
-        nextUri={props.pageContext.next.fields.slug}
-      />
       <Container>
         <pre>56 {JSON.stringify(props, null, 2)}</pre>
       </Container>
@@ -59,10 +17,10 @@ const BlogPost:React.SFC<iBlogPostProps> = props => {
   )
 }
 
-export default BlogPost
+export default WebService
 
 export const pageQuery = graphql`
-  query BlogPostByID($id: String!) {
+  query WebServicePostByID($id: String!) {
     markdownRemark(id: { eq: $id }) {
       id
       html
