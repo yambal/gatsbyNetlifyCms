1cdf5767ce768d395dbd0197e83efcc8455d9424
June YAMAMOTO <june@yambal.net>
2019-08-07 00:16:30 +0900

    WebService Index

--- src/pages/web-services/index.tsx
+++ src/pages/web-services/index.tsx
@@ -4,13 +4,22 @@ import Helmet from 'react-helmet'
 import { Link, graphql } from 'gatsby'
 import Layout from '../../components/Layout'
 
-const WebServices = (props) => (
-  <Layout>
-    <section className="section">
-        <pre>{JSON.stringify(props, null, 2)}</pre>
-    </section>
-  </Layout>
-)
+const WebServices = (props) => {
+  const { edges } = props.data.allMarkdownRemark
+  return (
+    <Layout>
+      {edges.map((edge, index) => {
+        return (
+          <div>
+            <Link to={edge.node.fields.slug}>{edge.node.frontmatter.serviceNameJa}</Link>
+            <p>{edge.node.frontmatter.catch}</p>
+            <pre>{JSON.stringify(edge, null, 2)}</pre>
+          </div>
+        )
+      })}
+    </Layout>
+  )
+}
 
 export default WebServices
 
@@ -24,6 +33,10 @@ export const WebServiceQuery = graphql`
     allMarkdownRemark(limit: 1000, filter: {frontmatter: {templateKey: {eq: "web-services"}}}) {
       edges {
         node {
+          id
+          fields {
+            slug
+          }
           frontmatter {
             serviceNameJa
             tags
@@ -35,7 +48,6 @@ export const WebServiceQuery = graphql`
             }
             catch
           }
-          id
         }
       }
     }
