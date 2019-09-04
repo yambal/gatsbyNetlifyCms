4c6f28db75e9c303f9da64aa70ae1c354c77f758
June YAMAMOTO <june@yambal.net>
2019-09-04 22:42:06 +0900

    Vibrant

--- src/pages/web-services/index.tsx
+++ src/pages/web-services/index.tsx
@@ -3,20 +3,51 @@ import { kebabCase } from 'lodash'
 import Helmet from 'react-helmet'
 import { Link, graphql } from 'gatsby'
 import Layout from '../../components/Layout'
+import Container from '../../components/Container';
+import Img from "gatsby-image"
+
+interface colors {
+  vibrant: string
+  muted: string
+  lightVibrant: string
+  lightMuted: string
+  darkVibrant: string
+  darkMuted: string
+}
 
 const WebServices = (props) => {
   const { edges } = props.data.allMarkdownRemark
+
   return (
     <Layout>
-      {edges.map((edge, index) => {
-        return (
-          <div>
-            <Link to={edge.node.fields.slug}>{edge.node.frontmatter.serviceNameJa}</Link>
-            <p>{edge.node.frontmatter.catch}</p>
-            <pre>{JSON.stringify(edge, null, 2)}</pre>
-          </div>
-        )
-      })}
+      <Container>
+        {edges.map((edge, index) => {
+          const { colors: colors } = edge.node.frontmatter.featuredimage
+          return (
+            <div>
+              <Img
+                fluid={edge.node.frontmatter.featuredimage.childImageSharp.fluid}
+                objectFit="cover"
+                objectPosition="50% 50%"
+                alt=""
+                style={{ width: '150px', height: '150px' }}
+              />
+              <Link to={edge.node.fields.slug}>{edge.node.frontmatter.serviceNameJa}</Link>
+
+              <span style={{ color: colors.vibrant }} >{colors.vibrant}</span>
+              <span style={{ color: colors.muted }} >{colors.muted}</span>
+              <span style={{ color: colors.lightVibrant }} >{colors.lightVibrant}</span>
+              <span style={{ color: colors.lightMuted }} >{colors.lightMuted}</span>
+              <span style={{ color: colors.darkVibrant }} >{colors.darkVibrant}</span>
+              <span style={{ color: colors.darkMuted }} >{colors.darkMuted}</span>
+
+              <p>{edge.node.frontmatter.catch}</p>
+              <pre>{JSON.stringify(edge, null, 2)}</pre>
+              <pre>{JSON.stringify(colors, null, 2)}</pre>
+            </div>
+          )
+        })}
+      </Container>
     </Layout>
   )
 }
@@ -42,9 +73,19 @@ export const WebServiceQuery = graphql`
             tags
             featuredimage {
               childImageSharp {
-                id
+                fluid(maxWidth: 1280) {
+                  ...GatsbyImageSharpFluid_noBase64
+                }
               }
               publicURL
+              colors {
+                vibrant
+                muted
+                lightVibrant
+                lightMuted
+                darkVibrant
+                darkMuted
+              }
             }
             catch
           }
